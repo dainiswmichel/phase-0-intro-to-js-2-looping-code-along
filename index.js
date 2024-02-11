7c5e997f692aee22830254772e92000137dc3fec
Dainis W. Michel <dainis@dainis.net>
2024-02-11 18:55:38 +0100

    Looping Lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+
+function writeCards(names) {
+    return names.map(name => `Thank you, ${name}, for the wonderful surprise gift!`);
+  }
+  
+  function countDown(startingNumber) { 
+    for (let i = startingNumber; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  module.exports = {
+    writeCards,
+    countDown,
+  };
+  
